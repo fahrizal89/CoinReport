47715a59e13cbbc029ac8ac94f8517a5c00e8142
Fahrizal Sentosa <fahrizal.sentosa@dana.id>
2022-06-24 05:59:36 +0700

    Add CoinPriceWorker

--- app/src/main/java/com/fahrizal/coinreport/App.kt
+++ app/src/main/java/com/fahrizal/coinreport/App.kt
@@ -1,16 +1,22 @@
 package com.fahrizal.coinreport
 
 import android.app.Application
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class App :Application(){
+class App : Application(), Configuration.Provider {
 
     override fun onCreate() {
         super.onCreate()
         initLogger()
     }
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setMinimumLoggingLevel(android.util.Log.INFO)
+            .build()
 
     private fun initLogger() = Timber.plant(Timber.DebugTree())
 }
---
+++ app/src/main/java/com/fahrizal/coinreport/ui/worker/CoinPriceWorker.kt
@@ -0,0 +1,31 @@
+package com.fahrizal.coinreport.ui.worker
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.fahrizal.coinreport.dispatcher.CoroutineDispatcherProvider
+import com.fahrizal.coinreport.domain.usecase.GetCoinPriceUseCase
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+
+@HiltWorker
+class CoinPriceWorker @AssistedInject constructor(
+    @Assisted appContext: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val coroutineDispatcherProvider: CoroutineDispatcherProvider,
+    private val getCoinPriceUseCase: GetCoinPriceUseCase
+) : CoroutineWorker(appContext, workerParams) {
+
+    override suspend fun doWork(): Result {
+        withContext(coroutineDispatcherProvider.io) {
+            getCoinPriceUseCase.invoke(true).collect {
+                Timber.d("CoinPriceWorker Fetch Success")
+            }
+        }
+
+        return Result.success()
+    }
+}
