b592a42f77b15a92d7154873c9c457a5d77ec1e0
Fahrizal Sentosa <fahrizal.sentosa@dana.id>
2022-06-27 01:04:35 +0700

    Add location permissions

--- app/src/main/java/com/fahrizal/coinreport/ui/MainActivity.kt
+++ app/src/main/java/com/fahrizal/coinreport/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.fahrizal.coinreport.ui
 
-import android.Manifest
-import android.content.Intent
+import android.Manifest.permission.*
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -18,31 +18,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (isMapPermitted()) {
-            getLocation()
+        checkLocationPermissions()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        checkLocationPermissions()
+    }
+
+    private fun checkLocationPermissions() {
+        val permissions = arrayOf(ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION)
+
+        if (isAllowedToAccess(permissions, PERMISSION_MAP_CODE)) {
+            isBackgroundLocationPermitted()
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        getLocation()
-    }
-
-    private fun getLocation() {
-        viewModel.getLocation()
-    }
-
-    private fun isMapPermitted(): Boolean {
-        val permissions = arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        )
-
-        return isAllowedToAccess(permissions, PERMISSION_MAP_CODE)
+    private fun isBackgroundLocationPermitted(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            isAllowedToAccess(ACCESS_BACKGROUND_LOCATION, PERMISSION_BACKGROUND_MAP_CODE)
+        } else {
+            return true
+        }
     }
 
     companion object {
 
-        const val PERMISSION_MAP_CODE = 1000
+        private const val PERMISSION_MAP_CODE = 1000
+        private const val PERMISSION_BACKGROUND_MAP_CODE = 1003
     }
 }
